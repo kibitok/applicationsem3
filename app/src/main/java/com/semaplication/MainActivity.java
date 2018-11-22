8d9545b2ad31bebb62cfcd2596170f0b17780fec
WanguM <susanwangu@gmail.com>
2018-11-22 16:21:45 +0300

    typography and ui interface

--- app/src/main/java/com/semaplication/MainActivity.java
+++ app/src/main/java/com/semaplication/MainActivity.java
@@ -4,6 +4,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,4 +31,55 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+
+        inflater.inflate(R.menu.options, menu);
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+//respond to menu item selection
+        switch (item.getItemId()) {
+
+            case R.id.settings:
+
+                Toast.makeText(this,"Settings", Toast.LENGTH_LONG).show();
+
+                return true;
+
+            case R.id.about:
+
+                Toast.makeText(this,"about", Toast.LENGTH_LONG).show();
+
+                return true;
+            case R.id.help:
+
+                Toast.makeText(this,"about", Toast.LENGTH_LONG).show();
+
+                return true;
+            default:
+
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public void settings(MenuItem item) {
+        Toast.makeText(this,"Settings", Toast.LENGTH_LONG).show();
+    }
+    public void about(MenuItem item) {
+        Toast.makeText(this,"about", Toast.LENGTH_LONG).show();
+    }
+    public void help(MenuItem item) {
+        Toast.makeText(this,"help", Toast.LENGTH_LONG).show();
+    }
 }
+
